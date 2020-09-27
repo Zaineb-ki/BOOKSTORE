d14466bf492725c07ba04bf7c2246835db7563c3
KOSSOURI Zaïneb <kossorizaineb@gmail.com>
2020-09-27 19:51:24 +0100

    Version Fini V1.0

--- ecommerce/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //Définir les paramétres de notre base de données
-    define ("DB_USER", "root"); 
-    define ("DB_PASSWORD", ""); 
-    define ("HOST", "localhost");
-    define ("DB_NAME", "bookstore-ecommerce");
-
-?>
---
+++ views/contact.php
@@ -0,0 +1,65 @@
+<!-- Part 1--> 
+<section class="part1-NousC">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <center>
+                    <div id="bar">
+                        <p>Nous Cantacter </p>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="part2-NousC">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <center>
+                    <div class="h5-NousC">
+                        <h5>
+                            N'hésitez surtout pas à nous contacter si vous ne trouvez pas les réponses que vous cherchez ici dans 
+                            la section Service Client BOOK STORE. Nous serons ravis de vous répondre.
+                        </h5>
+                    </div>
+                </center>
+            </div>
+        </div>
+    </div>
+</section>
+<br> <br>
+<section class="part3-NousC">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <img src="public\uploads\Images\img\4067830.png" alt="">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <p class="p-NousC" style=" color: #464545; font-family: Josefin Sans, Regular Roman;">Veuillez remplir le formulaire ci-dessous si vous souhaitez nous envoyer 
+                    un email. Vous êtes également invités à nous envoyer votre question 
+                    directement à <strong>bookstorecompany1@gmail.com</strong> .
+                    Nous vous répondrons dans les 24 heures.</p>
+                  
+                    <div class="formContact">
+                    <form id="formContact">
+                        <div class="form-group">
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nom et prénom ">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email ">
+                        </div>
+                        <div class="form-group">
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Sujet  ">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Votre Message" style="height: 150px;">
+                          </div>
+                        
+                        <button type="submit" class="btn btn-primary" id="Envoyer-NousC">Envoyer</button>
+                    </form>
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
